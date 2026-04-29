21c2472c95259ba43c4d9ecf1a06cc65017af2b2
IgorCristhofer <igorcristhoferam@gmail.com>
2026-04-29 16:10:41 -0300

    ex-001

---
+++ exercicios/ex001-criaçãoComVite/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  //área de código do componente App, onde podemos escrever lógica e retornar o JSX que será renderizado na tela.
+  return (
+    //área de renderização do componente.
+    <>
+        <h1>Primeira criação com Vite</h1>
+        <p>Esse é um projeto criado com Vite, uma ferramenta de construção rápida e leve para projetos web modernos. Ele oferece uma experiência de desenvolvimento otimizada, com recarregamento instantâneo e suporte a módulos ES. Com Vite, você pode criar aplicativos web de forma eficiente e aproveitar as últimas tecnologias do ecossistema JavaScript.</p>
+    </>
+  )
+}
+
+export default App
